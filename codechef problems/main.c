d42252a3b891c052a4a9c4c4d1fcb0c80f07bdc6
gunjan <singhalgunjan5@gmail.com>
2021-05-09 13:11:03 +0530

    Initial Commit

---
+++ codechef problems/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  codechef problems
+//
+//  Created by apple on 09/05/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
